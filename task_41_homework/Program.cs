885063da5526d05bcf3aeac14f960925068badfc
deardaddy <sergpomorsky@gmail.com>
2022-07-05 22:17:32 +0300

    Add task 41 homework

---
+++ task_41_homework/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество чисел M");
+int M = Convert.ToInt32(Console.ReadLine());
+int[]array = new int [M];
+for (int i = 0; i < M; i++)
+{
+    Console.Write($"Введите {i+1}-е число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int sum = 0;
+for(int i = 0; i < array.Length; i++)
+if (array[i] > 0)
+{
+    sum = sum + 1;
+}
+Console.WriteLine($"Чисел больше 0 = {sum}");
