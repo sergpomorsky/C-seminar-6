a483f7ab1ffd2086c6e17a9c175a1be56c522b39
deardaddy <sergpomorsky@gmail.com>
2022-07-06 17:03:25 +0300

    add update task 43

--- task_43/Program.cs
+++ task_43/Program.cs
@@ -12,6 +12,9 @@ double b2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите значение k2 = ");
 double k2 = Convert.ToInt32(Console.ReadLine());
 double x = (b2 - b1) / (k1 - k2);
-double y1 = k1 * x + b1;
-double y2 = k2 * x + b2;
-Console.WriteLine($"y = {y1} ; {y2}");
+double y = k1 * x + b1;
+Console.WriteLine($"x = {x} ; y = {y} ");
+if (k1 == k2)
+Console.WriteLine ("Прямые параллельны");
+if (k1 == k2 && b1 == b2)
+Console.WriteLine ("Прямые совпадают");
